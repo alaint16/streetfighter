e68e97d7c1c3dcbc7367d2fb561ef6100133d050
alaint16 <atovar@optimeconsulting.com>
2016-07-05 17:22:55 -0400

    changes on js file

--- js/app.js
+++ js/app.js
@@ -12,19 +12,19 @@ $(document).ready(function() {
   });
 
 
-  $('.ryu').click(function(){ alert ('helloooo');
+  $('.ryu').click(function(){
      $('.hadouken').show().animate(
      {'left': '1020px'}, 500,
      function(){
-     	$(this).hide();
-     	$(this).css('left','20px');
+      $(this).hide();
+      $(this).css('left','20px');
       console.log('this is working');
      })
 
   })
   .mousedown(function(){
-  	playHadouken();
-  	$('.ryu-throwing').show();
+    playHadouken();
+    $('.ryu-throwing').show();
     $('.ryu-cool').hide();
     $('.ryu-ready').hide();
     $('.ryu-still').hide();
@@ -42,25 +42,25 @@ $('.hadouken').finish().show()
 
   })
   .mouseup(function(){
-  	$('.hadouken').hide();
-  	$('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
+    $('.hadouken').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 
 
 
 $(document).keydown(function(x){
 if (x.keyCode == 88) {
-	$('.ryu-cool').show();
-	  	$('.ryu-still').hide();
-	  	$('.ryu-ready').hide();
-	  	$('.ryu-throwing').hide();
+  $('.ryu-cool').show();
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
 }
 
 });
 
 $(document).keyup(function(x){
-	if (x.keyCode == 88) {
+  if (x.keyCode == 88) {
 $('.ryu-cool').hide();
 $('.ryu-ready').hide();
 $('.ryu-throwing').hide();
@@ -74,7 +74,7 @@ $('.ryu-still').show();
 
 
 function playHadouken() {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
 }
